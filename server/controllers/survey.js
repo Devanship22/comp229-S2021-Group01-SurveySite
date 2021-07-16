dd71fd62b5ca213e6bdabd9162b0cd84581c2788
Tanisha Sharma <sharmatanisha1997@gmail.com>
2021-07-16 20:34:40 +0530

    implemented UPDATE operation for survey

--- server/controllers/survey.js
+++ server/controllers/survey.js
@@ -1,71 +1,109 @@
-let express = require('express');
-let router = express.Router();
-let mongoose = require('mongoose');
+let express = require("express");
 
 //connect to our survey model
-let Survey = require('../models/survey');
+let Survey = require("../models/survey");
 
 module.exports.displaySurveyList = (req, res, next) => {
-    Survey.find((err, surveyList) => {
-        if(err)
-        {
-            return console.error(err);
-        }
-        else
-        {
-            //console.log(surveyList);
-            res.render('survey/list', {title: 'Survey', surveyList: surveyList});
-        }
-       
-    });
- 
-}
+  Survey.find((err, surveyList) => {
+    if (err) {
+      return console.error(err);
+    } else {
+      //console.log(surveyList);
+      res.render("survey/list", { title: "Survey", surveyList: surveyList });
+    }
+  });
+};
 module.exports.displayAddPage = (req, res, next) => {
-    res.render('survey/add', {title: 'Add a Survey'})          
-}
+  res.render("survey/add", { title: "Add a Survey" });
+};
 
 module.exports.processAddPage = (req, res, next) => {
-  
-    let newSurvey = Survey({
-        "name": req.body.name,
-        "type": req.body.type,
-        "created": req.body.currentdate,
-        "expiry": req.body.expirydate,
-        "questions": req.body.questions,
-        "answer":req.body.answer
-          
+  const { name, type, currentdate, expirydate, questions, answer } = req.body;
+  const isValidRequest = validateSurveyData(req);
+  if (!isValidRequest) {
+    return res.redirect("back");
+  }
+  let newSurvey = Survey({
+    name: name,
+    type: type,
+    created: currentdate,
+    expiry: expirydate,
+    questions: questions,
+    answer: answer,
+  });
+
+  Survey.create(newSurvey, (err, Survey) => {
+    if (err) {
+      console.log(err);
+      res.end(err);
+    } else {
+      // refresh the book list
+      res.redirect("/survey-list");
+    }
+  });
+};
+module.exports.displayEditPage = async (req, res, next) => {
+  try {
+    const survey = await Survey.findById(req.params.id);
+    return res.render("survey/edit", { title: "Survey", survey });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports.processEditPage = async (req, res, next) => {
+  try {
+    const { name, type, currentdate, expirydate, questions, answer } = req.body;
+    console.log(req.body);
+    const isValidRequest = validateSurveyData(req);
+    if (!isValidRequest) {
+      return res.redirect("back");
+    }
+    await Survey.findByIdAndUpdate(req.params.id, {
+      $set: {
+        name: name,
+        type: type,
+        created: currentdate,
+        expiry: expirydate,
+        questions: questions,
+        answer: answer,
+      },
     });
+    req.session.message = {
+      type: "success",
+      title: "Action success",
+      details: "Survey has been updated successfully",
+    };
+    res.redirect("/survey-list");
+  } catch (error) {
+    next(error);
+  }
+};
 
-    Survey.create(newSurvey, (err, Survey) =>{
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        else
-        {
-            // refresh the book list
-            res.redirect('/survey-list');
-        }
-    });
-    
+//Delete the survey
+module.exports.performDelete = async (req, res, next) => {
+  try {
+    await Survey.findByIdAndDelete(req.params.id);
+    res.redirect("/survey-list");
+  } catch (error) {
+    next(error);
+  }
+};
 
-}
-//Delete the survey 
-module.exports.performDelete = (req, res, next) => {
-    let id = req.params.id;
-
-    Survey.remove({_id: id}, (err) => {
-        if(err)
-        {
-            console.log(err);
-            res.end(err);
-        }
-        else
-        {
-            //refresh the survey list
-            res.redirect('/survey-list');
-        }
-
-    });
+function validateSurveyData(req) {
+  const { name, type, currentdate, expirydate, questions, answer } = req.body;
+  let errorInfo = "",
+    isValid = true;
+  if (!name || !type || !currentdate || !expirydate || !questions) {
+    errorInfo = "Missing required parameters";
+    isValid = false;
+  } else if (new Date(currentdate) > new Date(expirydate)) {
+    errorInfo = "Expiry date can't be less than current date";
+    isValid = false;
+  }
+  req.session.message = {
+    type: "danger",
+    title: "Action failed",
+    details: errorInfo,
+  };
+  return isValid;
 }
